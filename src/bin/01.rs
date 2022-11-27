e9ea962274b867056c679dbf040438e88f56c3a5
Eduardo Sánchez S <whipshout@gmail.com>
2022-11-27 18:37:48 +0100

    Initial commit

---
+++ src/bin/01.rs
@@ -0,0 +1,4 @@
+#[aoc::main(01)]
+fn main(input: &str) -> (usize, usize) {
+    (0, 0)
+}
---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::process::Command;
+
+use once_cell::sync::Lazy;
+use regex::Regex;
+use walkdir::WalkDir;
+
+static MS_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"Time: (\d+)ms").unwrap());
+
+fn extract_time(s: &str) -> u32 {
+    let capture = MS_REGEX.captures_iter(s).next().unwrap();
+    capture[1].parse().unwrap()
+}
+
+fn main() {
+    let number_of_problems = WalkDir::new("./src/bin/").into_iter().count() - 1;
+
+    let total_time = (1..=number_of_problems)
+        .map(|day_num| {
+            let day = format!("{:0>2}", day_num);
+            let cmd = Command::new("cargo")
+                .args(["run", "--release", "--bin", &day])
+                .output()
+                .unwrap();
+            let output = String::from_utf8(cmd.stdout).unwrap();
+            println!("Day {}:\n{}", day, output);
+            extract_time(&output)
+        })
+        .sum::<u32>();
+
+    println!("Total time: {}ms", total_time);
+}
